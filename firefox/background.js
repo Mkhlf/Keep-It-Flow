8b3dafbfd0d56c2d41b164549c256c7c3b989855
Mohammad Alkhalifah <56599163+Mkhlf@users.noreply.github.com>
2022-06-23 03:23:58 -0700

    Fix the bug when the selected tab is muted.

--- firefox/background.js
+++ firefox/background.js
@@ -1,5 +1,6 @@
 let mainTab = null;
 
+// get the current tabs and mute them if the maintab is active, and if not unmute them 
 async function querybrowser() {
   tab = await getTab(mainTab);
   if (tab) {
@@ -23,6 +24,7 @@ async function querybrowser() {
     });
 }
 
+// get the tab from the tab id
 async function getTab(tabId) {
   if (tabId === null) return null;
   if (typeof (tabId) !== `string` && typeof (tabId) !== `number`) { console.log(typeof (tabId)); return null; }
@@ -30,33 +32,20 @@ async function getTab(tabId) {
   return tab;
 }
 
+// take the tab to toggled, and the newstate
 async function toggleMute(tabId, state = true) {
   let currTab = await getTab(tabId);
   if (currTab === null || currTab.muted === state) return;
-  if (tabId === mainTab) return;
   await browser.tabs.update(tabId, { muted: state });
 }
 
-function tabsInfo(listOfTabs) {
-  browser.storage.local.set({ tabsReturned: listOfTabs }, function () {
-    console.log('Value is set to ' + listOfTabs);
-  });
-}
-
-
-function keepItFlow() {
-  id = browser.storage.local.get(['titleID'], function (result) {
-    browser.tabs.query({ audible: true }, function (tabs) {
-      console.log(tabs);
-    });
-  });
-}
 
 // Set the initial empty tab
 browser.storage.local.set({ choice: null });
 
 // when the user select a new tab, chage it to the current tab
 browser.storage.onChanged.addListener(async function (changes) {
+
   console.log(changes['choice']);
   var newTitle = changes['choice']['newValue'];
   var oldTitle = changes['choice']['oldValue'];
@@ -72,24 +61,21 @@ browser.storage.onChanged.addListener(async function (changes) {
     mainTab = parseInt(newTitle);
     // mainTab.addEventListener('onUpdated', keepItFlow());
     console.log('mainTap is :', mainTab);
+    toggleMute(mainTab, false);
     await querybrowser();
   }
 });
 
-// calling querybrowser
+
+// calling if a new tab is created, add it and mute it if needed.
 browser.tabs.onCreated.addListener(async function () {
   await querybrowser();
 });
-// browser.tabs.onRemoved.addListener(function () {
-//   // check if the deleted tab is the mainTab
-//   querybrowser();
-// });
-// browser.tabs.onMoved.addListener(function () {
-//   querybrowser();
-// });
-const filter = {
-  properties: ["audible"]
-};
+// @TODO: 
+browser.tabs.onRemoved.addListener(function () {
+  // check if the deleted tab is the mainTab
+});
+
 // only check if the maintab were chenged
 browser.tabs.onUpdated.addListener(async function (tabid) {
   mainTab = await browser.storage.local.get('choice');
@@ -98,6 +84,8 @@ browser.tabs.onUpdated.addListener(async function (tabid) {
   mainTab = parseInt(mainTab);
   console.log('updated', tabid, mainTab);
   await querybrowser();
-}, filter);
+}, {
+  properties: ["audible"]
+});
 // calling the main functions
 querybrowser();
